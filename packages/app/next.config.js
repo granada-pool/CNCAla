c75a2321802b1c45294aee6fb755473a773cc1bf
VitoMelchi <vitomelchionn@gmail.com>
2022-11-13 23:07:00 +0100

    Added library to allow global css imports from node modules

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+const { patchWebpackConfig } = require('next-global-css')
+
 const nextConfig = {
 	reactStrictMode: true,
+	webpack: (config, options) => {
+		patchWebpackConfig(config, options)
+	
+		if (options.isServer) {
+		  config.externals = webpackNodeExternals({
+			// Uses list to add this modules for server bundle and process.
+			allowlist: [/design-system/],
+		  })
+		}
+	
+		return config
+	},
 	swcMinify: true,
 	/**
 	 * Note that this can change/break without warning.
