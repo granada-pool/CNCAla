eb0926e53377a02c6d099ac70f5b48ab6a32fd1b
VitoMelchi <vitomelchionn@gmail.com>
2022-11-14 18:45:13 +0100

    removed workaround library

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,28 +1,14 @@
 /** @type {import('next').NextConfig} */
-const { patchWebpackConfig } = require('next-global-css')
 
 const nextConfig = {
 	reactStrictMode: true,
-	webpack: (config, options) => {
-		patchWebpackConfig(config, options)
-	
-		if (options.isServer) {
-		  config.externals = webpackNodeExternals({
-			// Uses list to add this modules for server bundle and process.
-			allowlist: [/design-system/],
-		  })
-		}
-	
-		return config
-	},
 	swcMinify: true,
 	/**
 	 * Note that this can change/break without warning.
 	 * @see https://github.com/vercel/next.js/pull/22867
 	 */
 	experimental: {
-		externalDir: true,
-		appDir: true
+		externalDir: true
 	}
 };
 
