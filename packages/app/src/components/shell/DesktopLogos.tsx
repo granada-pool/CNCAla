0598eb0cb84b37a83972a31483fb6ee4dc868013
VitoMelchi <vitomelchionn@gmail.com>
2022-12-14 19:10:20 +0100

    Added links to partner logos

--- packages/app/src/components/shell/DesktopLogos.tsx
+++ packages/app/src/components/shell/DesktopLogos.tsx
@@ -1,58 +1,72 @@
 import React from "react";
-import { Image } from "@chakra-ui/react";
+import { Image, Link } from "@chakra-ui/react";
 
 export default function DesktopLogos() {
 	return (
 		<>
-			<Image
-				src={"/assets/images/Proxies_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"32"}
-			/>
-			<Image
-				src={"/assets/images/YummiUniverse_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"20"}
-			/>
-			<Image
-				src={"/assets/images/NEWM_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"16"}
-			/>
-			<Image
-				src={"/assets/images/cnc_logo_dark.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"52"}
-			/>
-			<Image
-				src={"/assets/images/Madalemurs_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"16"}
-			/>
-			<Image
-				src={"/assets/images/EarthNative_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"20"}
-			/>
-			<Image
-				src={"/assets/images/DripDropz_logo.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"32"}
-			/>
+			<Link href="https://cardanoproxies.io/">
+				<Image
+					src={"/assets/images/Proxies_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"32"}
+				/>
+			</Link>
+			<Link href="https://yummiuniverse.com/">
+				<Image
+					src={"/assets/images/YummiUniverse_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"20"}
+				/>
+			</Link>
+			<Link href="https://newm.io/">
+				<Image
+					src={"/assets/images/NEWM_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"16"}
+				/>
+			</Link>
+			<Link href="https://climateneutralcardano.org/">
+				<Image
+					src={"/assets/images/cnc_logo_dark.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"52"}
+				/>
+			</Link>
+			<Link href="https://twitter.com/madaslemurs">
+				<Image
+					src={"/assets/images/Madalemurs_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"16"}
+				/>
+			</Link>
+			<Link href="https://earthnatives.io/">
+				<Image
+					src={"/assets/images/EarthNative_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"20"}
+				/>
+			</Link>
+			<Link href="https://dripdropz.io/">
+				<Image
+					src={"/assets/images/DripDropz_logo.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"32"}
+				/>
+			</Link>
 		</>
 	);
 }
--- packages/app/src/components/shell/MobileLogos.tsx
+++ packages/app/src/components/shell/MobileLogos.tsx
@@ -1,74 +1,88 @@
 import React from "react";
-import { Center, Image, SimpleGrid } from "@chakra-ui/react";
+import { Center, Image, Link, SimpleGrid } from "@chakra-ui/react";
 
 export default function MobileLogos() {
 	return (
 		<>
-			<Image
-				src={"/assets/images/cnc_logo_dark.png"}
-				alt={"CNC"}
-				loading="lazy"
-				objectFit="contain"
-				w={"72"}
-			/>
+			<Link href="https://climateneutralcardano.org/">
+				<Image
+					src={"/assets/images/cnc_logo_dark.png"}
+					alt={"CNC"}
+					loading="lazy"
+					objectFit="contain"
+					w={"72"}
+				/>
+			</Link>
 			<SimpleGrid
 				columns={{ base: 1, md: 3 }}
 				gap={{ base: "5", md: "6" }}
 			>
 				<Center>
-					<Image
-						src={"/assets/images/YummiUniverse_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"20"}
-					/>
+					<Link href="https://yummiuniverse.com/">
+						<Image
+							src={"/assets/images/YummiUniverse_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"20"}
+						/>
+					</Link>
 				</Center>
 				<Center>
-					<Image
-						src={"/assets/images/Proxies_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"36"}
-					/>
+					<Link href="https://cardanoproxies.io/">
+						<Image
+							src={"/assets/images/Proxies_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"36"}
+						/>
+					</Link>
 				</Center>
 				<Center>
-					<Image
-						src={"/assets/images/EarthNative_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"20"}
-					/>
+					<Link href="https://earthnatives.io/">
+						<Image
+							src={"/assets/images/EarthNative_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"20"}
+						/>
+					</Link>
 				</Center>
 				<Center>
-					<Image
-						src={"/assets/images/NEWM_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"16"}
-					/>
+					<Link href="https://newm.io/">
+						<Image
+							src={"/assets/images/NEWM_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"16"}
+						/>
+					</Link>
 				</Center>
 				<Center>
-					<Image
-						src={"/assets/images/DripDropz_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"36"}
-						pt={"4"}
-					/>
+					<Link href="https://dripdropz.io/">
+						<Image
+							src={"/assets/images/DripDropz_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"36"}
+							pt={"4"}
+						/>
+					</Link>
 				</Center>
 				<Center>
-					<Image
-						src={"/assets/images/Madalemurs_logo.png"}
-						alt={"CNC"}
-						loading="lazy"
-						objectFit="contain"
-						w={"16"}
-					/>
+					<Link href="https://twitter.com/madaslemurs">
+						<Image
+							src={"/assets/images/Madalemurs_logo.png"}
+							alt={"CNC"}
+							loading="lazy"
+							objectFit="contain"
+							w={"16"}
+						/>
+					</Link>
 				</Center>
 			</SimpleGrid>
 		</>
